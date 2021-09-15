715b38da1b6f5e79b89358defc8a0d6b39e8f563
Leo <leonhard.schatt@gmx.de>
2021-09-15 11:38:47 +0200

    Pilze+Absorbtion

--- Versuch_Alpha_Gamma_Spektroskopie/Programme/Nebenrechnung/AbsortionskoeffizientAlu.py
+++ Versuch_Alpha_Gamma_Spektroskopie/Programme/Nebenrechnung/AbsortionskoeffizientAlu.py
@@ -23,10 +23,32 @@ popt, pcov = opt.curve_fit(Zerfall, df['Dicke'],df['Zaehlrate (1/s)'])
 
 perr = np.sqrt(np.diag(pcov))
 
-print(popt)
-print(perr)
+print('Linearer Absortionksoeffizient: '+str(popt[1]))
+print('Fehler linearer Absortionksoeffizient: '+str(perr[1]))
+
+
+fig = plt.figure()
+ax = fig.add_subplot(1, 1, 1)
+#ax.spines['left'].set_position('center')
+#ax.spines['bottom'].set_position('zero')
+ax.spines['right'].set_color('none')
+ax.spines['top'].set_color('none')
+#ax.xaxis.set_ticks_position('bottom')
+#ax.yaxis.set_ticks_position('left')
+#plt.ylim((0,0.5))
+
+
+#Bennenung des Plots
+plt.title('Gefittete Exponentialfunktion bei Aluminium')
+plt.xlabel('Dicke $d$ (cm)')
+plt.ylabel('Zählrate (cps)')
+
 
 #Plot machen
 plt.plot(df['Dicke'],df['Zaehlrate (1/s)'], marker = 'x', color = 'r', ls = 'None')
 plt.plot(np.linspace(0,df.iloc[-1,0]*1.1,100),Zerfall(np.linspace(0,df.iloc[-1,0]*1.1,100),popt[0],popt[1],popt[2]))
-plt.show()
+
+plt.errorbar(df['Dicke'],df['Zaehlrate (1/s)'],xerr= 0.1, yerr=popt[2],capsize = 2, ls = 'None',  color = 'black',elinewidth = 0.5)
+
+#plt.show()
+
--- Versuch_Alpha_Gamma_Spektroskopie/Programme/Umschreiben/MaximumFinden.py
+++ Versuch_Alpha_Gamma_Spektroskopie/Programme/Umschreiben/MaximumFinden.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 
-daten = 'Versuch_Alpha_Gamma_Spektroskopie/Umschreiben/EnergieeichungCS137.csv'
+daten = 'Versuch_Alpha_Gamma_Spektroskopie/Daten/CSV/Pilze.csv'
 df = pd.read_csv(daten)
 IndexMaxTopf = np.where(df['Count']== np.max(df['Count']))[0][0]  #Sucht Index
 MaxTopf = df.iloc[IndexMaxTopf,0]
