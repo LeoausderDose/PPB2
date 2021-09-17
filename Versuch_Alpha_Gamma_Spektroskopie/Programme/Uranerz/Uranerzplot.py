d7b5ef04161539c47e663e20c94b5b3292ed2e58
Leo <leonhard.schatt@gmx.de>
2021-09-17 08:37:50 +0200

    Rechnung und Einleitung

--- Versuch_Alpha_Gamma_Spektroskopie/Programme/Uranerz/Uranerzplot.py
+++ Versuch_Alpha_Gamma_Spektroskopie/Programme/Uranerz/Uranerzplot.py
@@ -1,20 +1,13 @@
 import numpy as np
 import pandas as pd
+#from scipy import optimize as opt
 import matplotlib.pyplot as plt
 
-daten = 'Versuch_Alpha_Gamma_Spektroskopie/Programme/Umschreiben/Energieeichungkorr.csv'
+
+daten = 'Versuch_Alpha_Gamma_Spektroskopie/Daten/CSV/Uranerz.csv'
 df = pd.read_csv(daten)
 
 
-
-parameter = np.polyfit(df['Topf'], df['Energie'],1)
-
-print(parameter)
-
-x = np.linspace(30, 800, 100)
-y = x*parameter[0]+parameter[1]
-
-#Plot an sich
 fig = plt.figure()
 ax = fig.add_subplot(1, 1, 1)
 #ax.spines['left'].set_position('center')
@@ -24,22 +17,20 @@ ax.spines['top'].set_color('none')
 #ax.xaxis.set_ticks_position('bottom')
 #ax.yaxis.set_ticks_position('left')
 #plt.ylim((0,0.5))
+#plt.xlim((0,:))
 
 
 #Bennenung des Plots
-plt.title('Energieeichgerade')
-plt.xlabel('Kanalnummer')
-plt.ylabel('Energie (eV)')
+plt.title('Gammaspektum Uranerz')
+plt.xlabel('Energie in MeV')
+plt.ylabel('Count (arb. u.)')
 
 
 #Plot machen
-plt.plot(df['Topf'], df['Energie'], color = 'r', marker = 'x', ls = 'None', label = 'Isotope')
-plt.plot(x,y, label = 'Eichgerade')
+plt.plot(df['Energie']/1e6,df['Count'], color = 'r')
 
 
 #plt.errorbar(df['Dicke'],df['Zaehlrate (1/s)'],xerr= 0.1, yerr=popt[2],capsize = 2, ls = 'None',  color = 'black',elinewidth = 0.5)
-plt.legend()
+
 plt.show()
-
-
 
